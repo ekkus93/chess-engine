63b9f07a867691c320746b6987153f07d49e18f0
Phillip Chin <ekkus93@gmail.com>
2026-08-04 12:32:11 -0700

    test: fix C ABI fuzz target entrypoint

--- fuzz/fuzz_targets/c_abi_buffers_handles.rs
+++ fuzz/fuzz_targets/c_abi_buffers_handles.rs
@@ -2,4 +2,4 @@
 
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| chess_engine_fuzz::fuzz_c_abi_buffers_handles(data));
+fuzz_target!(|data: &[u8]| chess_engine_fuzz::fuzz_c_abi_buffers_and_handles(data));
